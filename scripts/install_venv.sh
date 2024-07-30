22aa6de70b5a03ed45e0f8d9a465873a84a924d2
tresnax <tresna.widiyaman@gmail.com>
2024-07-30 13:16:34 +0700

    AWS service deploy

--- scripts/install_venv.sh
+++ scripts/install_venv.sh
@@ -2,7 +2,7 @@
 
 echo "Starting install_dependencies.sh" > /home/ubuntu/install_dependencies.log
 cd /home/ubuntu/django-app || { echo "Failed to change directory to /home/ubuntu/django-app"; exit 1; }
-python3 -m venv venv >> /home/ubuntu/install_dependencies.log 2>&1
+python3.8 -m venv venv >> /home/ubuntu/install_dependencies.log 2>&1
 source venv/bin/activate >> /home/ubuntu/install_dependencies.log 2>&1
 pip install -r requirements.txt >> /home/ubuntu/install_dependencies.log 2>&1
 echo "Finished install_dependencies.sh" >> /home/ubuntu/install_dependencies.log
--- scripts/unzip.sh
+++ scripts/unzip.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Starting unzip.sh" > /home/ubuntu/unzip.log
-unzip /home/ubuntu/django-app/django-app.zip -d /home/ubuntu/django-app >> /home/ubuntu/unzip.log 2>&1
+unzip django-app.zip -d /home/ubuntu/django-app >> /home/ubuntu/unzip.log 2>&1
 echo "Finished unzip.sh" >> /home/ubuntu/unzip.log
