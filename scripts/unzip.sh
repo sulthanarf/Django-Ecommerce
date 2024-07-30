4eaf0b5fa537939b5af63b8be021cd2797919d79
tresnax <tresna.widiyaman@gmail.com>
2024-07-30 09:46:07 +0700

    AWS service deploy

--- scripts/unzip.sh
+++ scripts/unzip.sh
@@ -2,5 +2,6 @@
 
 echo "Starting unzip.sh" > /home/ubuntu/unzip.log
 cd /home/ubuntu || { echo "Failed to change directory to /home/ubuntu"; exit 1; }
+chmod 777 django-app.zip
 unzip django-app.zip -d /home/ubuntu/django-app >> /home/ubuntu/unzip.log 2>&1
 echo "Finished unzip.sh" >> /home/ubuntu/unzip.log
