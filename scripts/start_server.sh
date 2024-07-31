04fd6a7642c6c4d84e31e779ecbd63525febe71d
tresnax <tresna.widiyaman@gmail.com>
2024-07-31 08:49:34 +0700

    penyesuaian code

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/django-app
 source venv/bin/activate
-python manage.py runserver 0.0.0.0:8000 >> /home/ubuntu/runserver.log 2>&1
+python manage.py runserver 0.0.0.0:8000
