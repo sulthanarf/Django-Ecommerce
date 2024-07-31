02897ba794088f92d23a5e2bc63f6a4db976a28b
tresnax <tresna.widiyaman@gmail.com>
2024-07-31 09:10:11 +0700

    penyesuaian code

--- scripts/install_venv.sh
+++ scripts/install_venv.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd /home/ubuntu/django-app
+cd /home/ubuntu/django-app || { echo "Failed to change directory to /home/ubuntu/django-app"; exit 1; }
 mkdir log
 echo "Starting install_dependencies.sh" > log/install_dependencies.log
-python3.8 -m venv venv >> log/install_dependencies.log 2>&1
+sudo python3.8 -m venv venv >> log/install_dependencies.log 2>&1
 source venv/bin/activate >> log/install_dependencies.log 2>&1
 pip install -r requirements.txt >> log/install_dependencies.log 2>&1
 echo "Finished install_dependencies.sh" >> log/install_dependencies.log
