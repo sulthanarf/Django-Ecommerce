6e7901861c67a3d586ad24aa5561d35df0fd26a9
tresnax <tresna.widiyaman@gmail.com>
2024-07-31 09:05:27 +0700

    penyesuaian code

--- scripts/install_venv.sh
+++ scripts/install_venv.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-sudo echo "Starting install_dependencies.sh" > /home/ubuntu/install_dependencies.log
-cd /home/ubuntu/django-app || { echo "Failed to change directory to /home/ubuntu/django-app"; exit 1; }
-sudo python3.8 -m venv venv >> /home/ubuntu/install_dependencies.log 2>&1
-source venv/bin/activate >> /home/ubuntu/install_dependencies.log 2>&1
-pip install -r requirements.txt >> /home/ubuntu/install_dependencies.log 2>&1
-echo "Finished install_dependencies.sh" >> /home/ubuntu/install_dependencies.log
+cd /home/ubuntu/django-app
+mkdir log
+echo "Starting install_dependencies.sh" > log/install_dependencies.log
+python3.8 -m venv venv >> log/install_dependencies.log 2>&1
+source venv/bin/activate >> log/install_dependencies.log 2>&1
+pip install -r requirements.txt >> log/install_dependencies.log 2>&1
+echo "Finished install_dependencies.sh" >> log/install_dependencies.log
--- scripts/unzip.sh
+++ scripts/unzip.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-sudo echo "Starting unzip.sh" > /home/ubuntu/unzip.log
-sudo unzip -o /home/ubuntu/django-app/django-app.zip -d /home/ubuntu/django-app >> /home/ubuntu/unzip.log 2>&1
-sudo echo "Finished unzip.sh" >> /home/ubuntu/unzip.log
+echo "Starting unzip.sh" > /home/ubuntu/unzip.log
+unzip -o /home/ubuntu/django-app/django-app.zip -d /home/ubuntu/django-app >> /home/ubuntu/unzip.log 2>&1
+echo "Finished unzip.sh" >> /home/ubuntu/unzip.log
+chown -R ubuntu:ubuntu /home/ubuntu/django-app/
