b905c9b8ffbcd106cbec77af407b6a0009898048
tresnax <tresna.widiyaman@gmail.com>
2024-07-31 09:52:03 +0700

    add gunicorn

--- scripts/unzip.sh
+++ scripts/unzip.sh
@@ -4,3 +4,4 @@ echo "Starting unzip.sh" > /home/ubuntu/unzip.log
 unzip -o /home/ubuntu/django-app/django-app.zip -d /home/ubuntu/django-app >> /home/ubuntu/unzip.log 2>&1
 echo "Finished unzip.sh" >> /home/ubuntu/unzip.log
 chown -R ubuntu:ubuntu /home/ubuntu/django-app
+killall gunicorn
